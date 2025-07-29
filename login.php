6b3c89ed9671a96b9828811585a04f272bbbeb35
Djrajeemaster <djrajeemix@gmail.com>
2025-07-29 01:17:41 -0400

    Add minified stylesheet

--- login.php
+++ login.php
@@ -27,6 +27,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <html>
 <head>
     <title>Login</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="style.css" />
 </head>
 <body>
 <h2>User Login</h2>
